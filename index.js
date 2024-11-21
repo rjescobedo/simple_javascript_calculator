709b288196a01dd1567c4e18ba355af9233c203f
Raul Escobedo <rjescobedo@gmail.com>
2024-11-21 05:46:47 -0600

    refactored code

--- index.js
+++ index.js
@@ -1,71 +1,96 @@
 const calculationsHistory = []; //Initializing an empty array where all of the objects will be placed
 
-/*Add function will only take 2 parameters. Created the equation for the function, 
-then created the added object, logging the result for the user, 
-and pushed the object into the calulationsHistory array*/
+/*Add function will only take 2 parameters. This function will calculate the formula, 
+log the equation and result for the user, and return.*/
 function add(num1, num2) {
     const added = num1 + num2;
-    const addedObject = {
-        operation: 'add', 
-        operands: [num1, num2],
-        result: added
-    };
-    console.log(num1, ' + ', num2, ' = ', added)
-    return calculationsHistory.push(addedObject);
-};
+    console.log(num1, ' + ', num2, ' = ', added);
+    return added;
+}
+/*The addToHistory function will take operation, operands, and result as their parameters. 
+These parameters will be used as keys as we create an object and push it to the calculationHistory array*/
+function addToHistory(operation, operands, result) {
+    calculationsHistory.push({
+        operation: operation,
+        operands: operands,
+        result: result
+    });
+}
 
-/*Subtract function will only take 2 parameters. Created the equation for the function, 
-then created the subtracted object, logging the result for the user, 
-and pushed the object into the calulationsHistory array*/
-function subtract(num1, num2){
+/*Subtract function will only take 2 parameters. This function will calculate the formula, 
+log the equation and result for the user, and return.*/
+function subtract(num1, num2) {
     const subtracted = num1 - num2;
-    const subtractedObject = {
-        operation: 'subtract',
-        operands: [num1, num2],
-        result: subtracted
-    };
-    console.log(num1, ' - ', num2, ' = ', subtracted)
-    return calculationsHistory.push(subtractedObject);
-};
+    console.log(num1, ' - ', num2, ' = ', subtracted);
+    return subtracted;
+}
+/*The subtractToHistory function will take operation, operands, and result as their parameters. 
+These parameters will be used as keys as we create an object and push it to the calculationHistory array*/
+function subtractToHistory(operation, operands, result) {
+    calculationsHistory.push({
+        operation: operation,
+        operands: operands,
+        result: result
+    });
+}
 
-/*Multiply function will only take 2 parameters. Created the equation for the function, 
-then created the multiplied object, logging the result for the user, 
-and pushed the object into the calulationsHistory array*/
+/*Multiply function will only take 2 parameters. This function will calculate the formula, 
+log the equation and result for the user, and return.*/
 function multiply(num1, num2) {
     const multiplied = num1 * num2;
-    const multipliedObject = {
-        operation: 'multiply',
-        operands: [num1, num2],
-        result: multiplied
-    };
-    console.log(num1, ' * ', num2, ' = ', multiplied)
-    return calculationsHistory.push(multipliedObject);
-};
+    console.log(num1, ' * ', num2, ' = ', multiplied);
+    return multiplied;
+}
+/*The multiplyToHistory function will take operation, operands, and result as their parameters. 
+These parameters will be used as keys as we create an object and push it to the calculationHistory array*/
+function multiplyToHistory(operation, operands, result) {
+    calculationsHistory.push({
+        operation: operation,
+        operands: operands,
+        result: result
+    });
+}
 
-/*Divide function will only take 2 parameters. Created the equation for the function, 
-if num2 equals to 0, then the console will log an error, else logging the result for the user
-and pushed the object into the calulationsHistory array*/
+/*Divide function will only take 2 parameters. This function will calculate the formula, 
+log the equation and result for the user, and return. If num2 equals to 0, we will log an error. Else, it will return the result of the equation*/
 function divide(num1, num2) {
     const divided = num1 / num2;
     if(num2 === 0) {
-        return console.error('Error! Not able to divide by 0');
+        console.error('Error! Not able to divide by 0.')
     } else {
-        const dividedObject = {
-            operation: 'divide',
-            operands: [num1, num2],
-            result: divided
+        console.log(num1, ' / ', num2, ' = ', divided);
+        return divided;
     };
-    console.log(num1, ' / ', num2, ' = ', divided)
-    return calculationsHistory.push(dividedObject);
-    };
-};
+}
+/*The divideToHistory function will take operation, operands, and result as their parameters. 
+These parameters will be used as keys as we create an object and push it to the calculationHistory array. If the second operand does not equal to 0,
+it will push the object to the calculationsHisotyr array. If it does equal to 0, nothing will be pushed into the array*/
+function divideToHistory(operation, operands, result) {
+    if(operands[1] !== 0) {
+        calculationsHistory.push({
+            operation: operation,
+            operands: operands,
+            result: result
+        });
+    }
+}
 
-/*Callback functions to test my simple JavaScript Calculator. Uncomment each callback function to test our calculator*/
-//add(2,10);
-//subtract(9, 24);
-//multiply(7,22);
-//divide(11, 0);
-//divide(12, 2);
+//Callback functions to test my simple JavaScript Calculator.
+let addResult = add(2, 10);
+addToHistory('add', [2, 10], addResult);
+
+let subtractResult = subtract(9, 24);
+subtractToHistory('subtract', [9, 24], subtractResult);
+
+let multiplyResult = multiply(7, 22);
+multiplyToHistory('multiply', [7,22], multiplyResult);
+
+//This should output as an error and should not be pushed into the calculationsHistory array
+let divideResult = divide(11, 0);
+divideToHistory('divide', [11, 0], divideResult);
+
+divideResult = divide(12, 2);
+divideToHistory('divide', [12, 2], divideResult)
+
 //This will log out the calculationsHistory array for the user
 console.log('Calculation History: ', calculationsHistory);
-
